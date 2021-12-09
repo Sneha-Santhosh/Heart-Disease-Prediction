bf1e6b325bd4c523153f2a94c12ab23987d9e9c7
ss <45783979+Sneha-Santhosh@users.noreply.github.com>
2021-12-08 22:29:13 -0500

    UI changes to predict page

--- react-client/src/components/Predict.js
+++ react-client/src/components/Predict.js
@@ -44,74 +44,36 @@ const Predict = (props) => {
       <Jumbotron classname='color-form'>
       <Form className="register-form" onSubmit={handleOnSubmit}>
         <Form.Group controlId="username">
-        <Form.Label>Age </Form.Label>&nbsp;
-          <input type="text" id="age" name="age" onChange={handleInputChange}></input><br></br>
-          <Form.Label>Male </Form.Label>&nbsp;
-        <input type="checkbox" id="male" name="male" value="1" onChange={handleInputChange}></input><br></br>
-        {/* <Form.Label>Have Chest Pain </Form.Label>&nbsp;
-        <input type="checkbox" id="chestPain" name="cp" onChange={handleInputChange}></input><br></br> */}
-        <Form.Label>Chest pain</Form.Label>
-          <Form.Control
-            type="number"
-            step="any"
-            placeholder="Chest pain type"
-            name="cp"
-            onChange={handleInputChange}
-          />
-          <Form.Label>Resting Blood Pressure</Form.Label>
-          <Form.Control
-            type="number"
-            step="any"
-            placeholder="mmHg"
-            name="trestbps"
-            onChange={handleInputChange}
-          />
+          <Form.Label>Age: </Form.Label>&nbsp;
+            <input type="text" id="age" name="age" onChange={handleInputChange} /><br></br>
+          <Form.Label>Male: </Form.Label>&nbsp;
+            <input type="checkbox" id="male" name="male" value="1" onChange={handleInputChange} /><br></br>
+          <Form.Label>Have Chest Pain: </Form.Label>&nbsp;
+            <input type="number" id="chestPain" name="cp" onChange={handleInputChange} /><br></br>
+          <Form.Label>Resting Blood Pressure: </Form.Label>
+            <input type="number" id="bloodPressure" name="bloodPressure" onChange={handleInputChange} /><br></br>
         </Form.Group>
-        <Form.Label>Cholesterol</Form.Label>
-          <Form.Control
-            type="number"
-            step="any"
-            placeholder="mg/dl"
-            name="chol"
-            onChange={handleInputChange}
-          />
-        <Form.Label>Fasting blood sugar greater than 120 mg/dl </Form.Label>&nbsp;
-        <input type="checkbox" id="fbs" name="fbs" value="1" onChange={handleInputChange}></input><br></br>
-        <Form.Label>Rest ecg </Form.Label>&nbsp;
-        <input type="checkbox" id="restecg" name="restecg" value="1" onChange={handleInputChange}></input><br></br>
-        <Form.Label>Max Heart Rate</Form.Label>
-          <Form.Control
-            type="number"
-            step="any"
-            placeholder="bpm"
-            name="thalach"
-            onChange={handleInputChange}
-          />
-        <Form.Label>Angina from exercise </Form.Label>&nbsp;
-        <input type="checkbox" id="exang" name="exang" value="1" onChange={handleInputChange}></input><br></br>
+        <Form.Group>
+          <Form.Label>Cholesterol: </Form.Label>&nbsp;
+            <input type="number" id="cholesterol" name="cholesterol" onChange={handleInputChange} /><br></br>
+          <Form.Label>Fasting blood sugar greater than 120 mg/dl: </Form.Label>&nbsp;
+            <input type="checkbox" id="fbs" name="fbs" value="1" onChange={handleInputChange} /><br></br>
+          <Form.Label>Rest ecg: </Form.Label>&nbsp;
+            <input type="checkbox" id="restecg" name="restecg" value="1" onChange={handleInputChange} /><br></br>
+          <Form.Label>Max Heart Rate: </Form.Label>&nbsp;
+            <input type="number" id="thalach" name="thalach" onChange={handleInputChange} /><br></br>
+          <Form.Label>Angina from exercise: </Form.Label>&nbsp;
+            <input type="checkbox" id="exang" name="exang" value="1" onChange={handleInputChange} /><br></br>
+        </Form.Group>
         <Form.Group controlId="epoch">
-          <Form.Label>Epoch</Form.Label>
-          <Form.Control
-            type="number"
-            step="any"
-            placeholder="Epoch"
-            name="epoch"
-            onChange={handleInputChange}
-          />
+          <Form.Label>Epoch: </Form.Label>
+            <input type="number" id="epoch" name="epoch" onChange={handleInputChange} /><br></br>
         </Form.Group>
         <Form.Group controlId="lr">
-          <Form.Label>Learning Rate</Form.Label>
-          <Form.Control
-            type="number"
-            step="any"
-            placeholder="Learning Rate"
-            name="lr"
-            onChange={handleInputChange}
-          />
+          <Form.Label>Learning Rate: </Form.Label>
+            <input text="number" id="lr" name="lr" onChange={handleInputChange} /><br></br>
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Predict
-        </Button>
+        <Button variant="primary" type="submit">Predict</Button>
       </Form>
    </Jumbotron>
     </div>
